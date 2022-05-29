8451dbfcf41420f1595b3e77539c7a411ea0e35a
Perrytheplaty43 <alexander.i.bakalov@gmail.com>
2022-05-29 00:59:03 -0700

    adding api functionality

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-import numpy as np
+import re
 from geographiclib.geodesic import Geodesic
 from global_land_mask import globe
+from numpy import float64
+from qiskit import IBMQ
 from qiskit_optimization.problems import QuadraticProgram
 from qiskit_optimization.algorithms import CplexOptimizer
+from qiskit_optimization.algorithms import MinimumEigenOptimizer
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import QAOA
+from qiskit.algorithms.optimizers import COBYLA
+from qiskit import Aer
+import flask
+from flask import request, jsonify
 import math
 
+# gcomp = Aer.get_backend('qasm_simulator')
+# qins = QuantumInstance(backend=gcomp, shots=100)
+# # qins = QuantumInstance(backend=Aer.get_backend("aer_simulator"), shots=100)
+# meo = MinimumEigenOptimizer(QAOA(COBYLA(maxiter=100), quantum_instance=qins))
 
-lat1 = 47.760111
+app = flask.Flask(__name__)
+
+lat1 = 0
 long1 = -122.205335
 
 lat2 = 47.0
@@ -17,66 +32,116 @@ ep = 33
 cp = 33
 sp = 33
 
-geod = Geodesic.WGS84
+@app.route('/', methods=['GET'])
+def getInfo():
+    global lat1
+    global long1
+    global lat2
+    global long2
+    global ton 
+    global ep
+    global cp
+    global sp
+    if 'lat1' in request.args:
+        lat1 = float64(request.args['lat1'])
+    else:
+        return "Error"
+    if 'long1' in request.args:
+        long1 = float64(request.args['long1'])
+    else:
+        return "Error"
+    if 'lat2' in request.args:
+        lat2 = float64(request.args['lat2'])
+    else:
+        return "Error"
+    if 'long2' in request.args:
+        long2 = float64(request.args['long2'])
+    else:
+        return "Error"
+    if 'ton' in request.args:
+        ton = float64(request.args['ton'])
+    else:
+        return "Error"
+    if 'ep' in request.args:
+        ep = float64(request.args['ep'])
+    else:
+        return "Error"
+    if 'cp' in request.args:
+        cp = float64(request.args['cp'])
+    else:
+        return "Error"
+    if 'sp' in request.args:
+        sp = float64(request.args['sp'])
+    else:
+        return "Error"
+    
+    geod = Geodesic.WGS84
+    l = geod.InverseLine(lat1, long1, lat2, long2)
+    ds = 1000
+    n = int(math.ceil(l.s13 / ds))
+    segments = []
+    curSeg = []
+    curSegState = False
 
-l = geod.InverseLine(lat1, long1, lat2, long2)
-ds = 1000
-n = int(math.ceil(l.s13 / ds))
-segments = []
-curSeg = []
-curSegState = False
+    kmWater = 0
+    kmLand = 0
 
-kmWater = 0
-kmLand = 0
+    for i in range(n + 1):
+        s = min(ds * i, l.s13)
+        g = l.Position(s, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
+        if i == 0:
+            curSegState = globe.is_land(g['lat2'], g['lon2'])
+            curSeg = [g['s12'], g['s12']]
+        if curSegState != globe.is_land(g['lat2'], g['lon2']):
+            curSeg.append(curSegState)
+            segments.append([(curSeg[1] - curSeg[0]) / 1000, curSeg[2]])
+            curSegState = globe.is_land(g['lat2'], g['lon2'])
+            curSeg = [g['s12'], g['s12']]
+        elif curSeg[1] < g['s12']:
+            curSeg[1] = g['s12']
 
-for i in range(n + 1):
-    s = min(ds * i, l.s13)
-    g = l.Position(s, Geodesic.STANDARD | Geodesic.LONG_UNROLL)
-    if i == 0:
-        curSegState = globe.is_land(g['lat2'], g['lon2'])
-        curSeg = [g['s12'], g['s12']]
-    if curSegState != globe.is_land(g['lat2'], g['lon2']):
-        curSeg.append(curSegState)
-        segments.append([(curSeg[1] - curSeg[0]) / 1000, curSeg[2]])
-        curSegState = globe.is_land(g['lat2'], g['lon2'])
-        curSeg = [g['s12'], g['s12']]
-    elif curSeg[1] < g['s12']:
-        curSeg[1] = g['s12']
+    for i in segments:
+        if i[1]:
+            kmLand += i[0]
+        elif not i[1]:
+            kmWater += i[0]
 
-for i in segments:
-    if i[1]:
-        kmLand += i[0]
-    elif not i[1]:
-        kmWater += i[0]
+    qp = QuadraticProgram()
 
-qp = QuadraticProgram()
+    m = (ep * (0.16 * kmLand * ton) + cp * (22 * kmLand * ton) * (1 / (sp * 15)))
+    w = kmWater
+    k = (ep * (0.18 * kmLand * ton) + cp * (62 * kmLand * ton) * (1 / (sp * 36)))
+    air = abs(kmLand - k - m + w)
 
-m = (ep * (0.16 * kmLand * ton) + cp * (22 * kmLand * ton) * (1 / (sp * 15)))
-w = kmWater
-k = (ep * (0.18 * kmLand * ton) + cp * (62 * kmLand * ton) * (1 / (sp * 36)))
-air = abs(kmLand - k - m + w)
+    KmMUpperBound = int(kmLand)
+    KmAUpperBound = int(kmLand + kmWater)
+    KmKUpperBound = int(kmLand)
+    KmBUpperBound = int(kmWater)
+    if ton > 400000:
+        KmBUpperBound = 0
+    if ton > 36:
+        KmKUpperBound = 0
+    if kmLand < 280 or ton > 12500:
+        KmMUpperBound = 0
+    if kmLand + kmWater < 280 or ton > 112:
+        KmAUpperBound = 0
+    print("Land: " + str(kmLand), "Water: " + str(kmWater))
+    # KmK = Truck
+    # KmM = Train
+    # KmB = boat
+    # KmA = KmA
+    qp.integer_var(name="KmK", lowerbound = 0, upperbound = KmKUpperBound)
+    qp.integer_var(name="KmM", lowerbound = 0, upperbound = KmMUpperBound)
+    qp.integer_var(name="KmB", upperbound = KmBUpperBound)
+    qp.integer_var(name="KmA", upperbound = KmAUpperBound)
+    qp.linear_constraint({"KmK": 1, "KmM": 1}, "<=", int(kmLand))
+    qp.linear_constraint({"KmK": 1, "KmM": 1, "KmB": 1, "KmA": 1}, "=", int(kmLand + kmWater))
 
-KmMUpperBound = int(kmLand)
-KmAUpperBound = int(kmLand + kmWater)
-if kmLand < 280:
-    KmMUpperBound = 0
-if kmLand + kmWater < 280:
-    KmAUpperBound = 0
-print("Land: " + str(kmLand), "Water: " + str(kmWater))
-# KmK = Truck
-# KmM = Train
-# KmB = boat
-# KmA = KmA
-qp.integer_var(name="KmK", lowerbound = 0, upperbound = int(kmLand))
-qp.integer_var(name="KmM", lowerbound = 0, upperbound = KmMUpperBound)
-qp.integer_var(name="KmB", upperbound = int(kmWater))
-qp.integer_var(name="KmA", upperbound = KmAUpperBound)
-qp.linear_constraint({"KmK": 1, "KmM": 1}, "<=", int(kmLand))
-qp.linear_constraint({"KmK": 1, "KmM": 1, "KmB": 1, "KmA": 1}, "=", int(kmLand + kmWater))
-
-print(w, k, m, air)
-if kmWater > 0:
-    qp.minimize(linear={"KmB": w, "KmK": k, "KmM": m, "KmA": air})
-else:
-    qp.minimize({"KmK": k, "KmM": m, "KmA": (kmLand - k - m + w)})
-print(CplexOptimizer().solve(qp))
+    print(w, k, m, air)
+    if kmWater > 0:
+        qp.minimize(linear={"KmB": w, "KmK": k, "KmM": m, "KmA": air})
+    else:
+        qp.minimize({"KmK": k, "KmM": m, "KmA": (kmLand - k - m + w)})
+    solution = CplexOptimizer().solve(qp)
+    result = re.search('[(.*)]', solution)
+    print(result.group(1))
